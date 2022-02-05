a2eaef674c3afc41da91d78ded0bb96f7b36210b
NikZhukov2505 <unnormaldj@gmail.com>
2022-02-05 12:03:18 +0600

    changes on code

--- src/API/index.jsx
+++ src/API/index.jsx
@@ -2,7 +2,7 @@ import axios from "axios"
 import { useParams } from "react-router-dom"
 
 const API = 'https://youtube.googleapis.com/youtube/v3/'
-const KEY = '&key=AIzaSyBSf_1Loo4IE_YD59-1BAphNu2wo4U1u1Q'
+const KEY = '&key=AIzaSyACxWmywexUO2PdB-N7BdEKriLQZcdyRCM'
 const RESULTS = 20
 const GET_ALL_VIDEO = API + 'search?&part=snippet&maxResults=' + RESULTS + '&type=video' + KEY + '&q='
 const GET_INFO_BY_ID = API + 'videos?part=snippet%2CcontentDetails%2Cstatistics' + KEY + '&id='
